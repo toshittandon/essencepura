cb6d4bbd49a24a3210cb0f1c7fce70645725b2f2
toshittandon <toshittandon9@gmail.com>
2025-08-26 01:16:46 +0200

    fix13

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,9 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "essencepura-84i8b.ondigitalocean.app",
+    host: "essencepura-b418b.ondigitalocean.app",
     port: 8080,
+    allowedHosts: ["essencepura-b418b.ondigitalocean.app"],
   },
   plugins: [
     react(),
